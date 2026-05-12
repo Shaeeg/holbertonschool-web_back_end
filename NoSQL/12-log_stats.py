36d2308a98f3bcdc8dd0452efeaa8c81bb46f257
“Shaeeg” <m.shaeeg@gmail.com>
2026-05-12 22:26:33 +0100

    Fix log_stats recursion and method filter for task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
-""" Print Nginx log statistics from MongoDB (database logs, collection nginx). """
-
+"""Module for improved version of log_stats function"""
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    nginx = client.logs.nginx
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+PIPE = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}, {"$limit": 10}]
 
-    print("{} logs".format(nginx.count_documents({})))
+
+def log_stats(mongo_collection, option=None):
+    """Function for logging stats"""
+    items = {}
+    if option:
+        value = mongo_collection.count_documents({"method": option})
+        print(f"\tmethod {option}: {value}")
+        return
+
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        print("    method {}: {}".format(
-            method,
-            nginx.count_documents({"method": method}),
-        ))
-    print("{} status check".format(
-        nginx.count_documents({"method": "GET", "path": "/status"}),
-    ))
+    for method in METHODS:
+        log_stats(mongo_collection, method)
+    status_check = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+    print("IPs:")
+
+    for ip in mongo_collection.aggregate(PIPE):
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+
+
+if __name__ == "__main__":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
